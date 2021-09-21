50a865690ed7ba4121275503814de7b65e9cfaaf
Sanjar Esonmurordov <sanjar.esonmurordov@fido.uz>
2021-09-21 07:29:59 -0700

    Initial Commit

---
+++ src/main/java/com/insure/demo/InsureProgramApplication.java
@@ -0,0 +1,13 @@
+package com.insure.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsureProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsureProgramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/insure/demo/InsureProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insure.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsureProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
